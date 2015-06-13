6466166156d916fdd150352461fe8aeb3eda6a88
Devashish Datt Mamgain <devashish.mamgain@mobitexter.net>
2015-06-13 13:00:34 +0530

    #39 Removed unused variables.

--- android/mobicomkit/src/main/java/com/mobicomkit/GeneralConstants.java
+++ android/mobicomkit/src/main/java/com/mobicomkit/GeneralConstants.java
@@ -6,4 +6,5 @@ public class GeneralConstants {
 
     public static final String SHARED_PREFERENCE_VERSION_UPDATE_KEY = "mck.version.update";
 
+    public static final String MOBITEXTER_SUPPORT_DIAL_PHONE_NUMBER = "+919535008745";
 }
