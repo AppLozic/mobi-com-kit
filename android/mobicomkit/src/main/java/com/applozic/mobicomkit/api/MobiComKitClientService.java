d4dd7354a160fa20de35391ee5d8aabba4c4af58
sunilkumarr <sunil588.k@gmail.com>
2015-08-08 19:05:49 +0530

    #110 Adding Header

--- android/mobicomkit/src/main/java/com/applozic/mobicomkit/api/MobiComKitClientService.java
+++ android/mobicomkit/src/main/java/com/applozic/mobicomkit/api/MobiComKitClientService.java
@@ -71,6 +71,7 @@ public class MobiComKitClientService {
             String basicAuth = "Basic " + Base64.encodeToString(userCredentials.getBytes(), Base64.NO_WRAP);
             httpConn.setRequestProperty("Authorization", basicAuth);
             httpConn.setRequestProperty(APPLICATION_KEY_HEADER, getApplicationKey(context));
+            httpConn.setRequestProperty(HttpRequestUtils.USERID_HEADER, HttpRequestUtils.USERID_HEADER_VALUE);
             httpConn.connect();
             //Shifting this Code to individual class..this is needed so that caller can decide ..what should be done with the error
 //            response = httpConn.getResponseCode();
